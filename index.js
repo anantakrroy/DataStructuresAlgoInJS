ea047001a7fd90329754f838344cac542e24850a
Ananta Kr <kr.innovate@gmail.com>
2020-02-19 14:48:43 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,13 @@
+// Stacks 
+
+const Stack = function() {
+  this.storage = {}
+  this.count = 0
+
+  // Push
+  this.push = function(value) {
+    this.count++
+    this.storage[]
+    return this.storage
+  }
+}
